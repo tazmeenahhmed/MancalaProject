9a2884b2166e2b2d80ac56a0b04c7515a91207d8
tazmeenahhmed <tazmeenahhmed@gmail.com>
2025-11-05 09:49:25 -0800

    Added some relevant variables and getter/setter methods to the pit and player classes

--- Pit.java
+++ Pit.java
@@ -1,1 +1,25 @@
-
+public class Pit {
+	
+	private String name;
+	private int stones;
+	
+	public Pit(String name, int stones) {
+		
+		this.name = name;
+		this.stones = stones;
+		
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getStones() {
+		return this.stones;
+	}
+	
+	public void setStones(int newStones) {
+		this.stones = newStones;
+	}
+	
+}
--- Player.java
+++ Player.java
@@ -1,1 +1,25 @@
 
+public class Player {
+	
+	private String name;
+	private boolean currentTurn;
+	
+	public Player(String name, boolean currentTurn) {
+		
+		this.name = name;
+		this.currentTurn = currentTurn;
+		
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public boolean getTurn() {
+		return this.currentTurn;
+	}
+	
+	public void setTurn(boolean turn) {
+		this.currentTurn = turn;
+	}
+}
