83f95ffef86393aeb370c17a442afd8f3044c129
Glengle Pham <92689013+GlenglePham@users.noreply.github.com>
2025-11-05 20:11:27 -0800

    Update UndoManager.java

--- UndoManager.java
+++ UndoManager.java
@@ -1,10 +1,23 @@
 
 public class UndoManager {
-	
+	/ I added varibles and method that connect to the pits and players - Glengle
+	private Stack<ArrayList<Pit>> undoStack = new Stack<>();
 	private Player currentPlayer;
     private boolean justUndid = false;
 
     public UndoManager(Player currentPlayer) {
         this.currentPlayer = currentPlayer;
+    }
+	public void saveState(ArrayList<Pit> currentState) {
+		 undoStack.push(copy);
+	}
+	public void resetUndo(Player nextPlayer) {
+		this.currentPlayer = nextPlayer;
+        this.justUndid = false;
+        this.undoStack.clear();
+        nextPlayer.setNumberOfUndos(0);
+	}
+	public boolean canUndo() {
+        return !justUndid && currentPlayer.getNumberOfUndos() < 3 && !undoStack.isEmpty();
     }
 }
