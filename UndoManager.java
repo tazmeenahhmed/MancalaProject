ceb1c981f669b39efdef99f568a41f788dcbd2f3
Glengle Pham <92689013+GlenglePham@users.noreply.github.com>
2025-11-05 19:50:06 -0800

    Update UndoManager.java

--- UndoManager.java
+++ UndoManager.java
@@ -1,5 +1,10 @@
 
 public class UndoManager {
 	
-	
+	private Player currentPlayer;
+    private boolean justUndid = false;
+
+    public UndoManager(Player currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
 }
