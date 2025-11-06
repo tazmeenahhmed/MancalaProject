1feeb137cd0d349027512d2e877af59f70aa5110
Glengle Pham <92689013+GlenglePham@users.noreply.github.com>
2025-11-05 20:12:27 -0800

    Update UndoManager.java

--- UndoManager.java
+++ UndoManager.java
@@ -1,6 +1,7 @@
 
 public class UndoManager {
 	/ I added varibles and method that connect to the pits and players - Glengle
+	Let me figure out this class. You give me suggestion in discord or comment the ideas
 	private Stack<ArrayList<Pit>> undoStack = new Stack<>();
 	private Player currentPlayer;
     private boolean justUndid = false;
