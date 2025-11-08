6fc80ea8462e29c02ad621ff7d56797996811469
Glengle Pham <92689013+GlenglePham@users.noreply.github.com>
2025-11-07 20:27:29 -0800

    Update BoardHexagon.java
    I am trying to create the board

--- BoardHexagon.java
+++ BoardHexagon.java
@@ -13,7 +13,15 @@ public class BoardHexagon implements BoardDesign{
 	@Override
 	public void draw(Graphics2D g2) {
 		// TODO Auto-generated method stub
+		this.pitList = model.getPitList();
 		
+		int pitWidth = 80;
+        int pitHeight = 120;
+        int spacing = 20;
+        int mancalaWidth = 80;
+        int mancalaHeight = 300;
+        int boardX = 300;
+        int boardY = 50;
 	}
 
 }
