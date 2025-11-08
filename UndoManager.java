23bd48a669fffb2a3565648c8f3b82ac4720722e
Glengle Pham <92689013+GlenglePham@users.noreply.github.com>
2025-11-07 19:40:04 -0800

    Update UndoManager.java
    I add new functions for the new methods and help with undo doing a turn and keeping track of player using their 3 undo turns

--- UndoManager.java
+++ UndoManager.java
@@ -12,9 +12,21 @@ public class UndoManager {
         this.currentPlayer = currentPlayer;
     }
 	public void saveState(ArrayList<Pit> currentState) {
-		 //undoStack.push("working varaible");
+		 if (currentPlayer.getNumberOfUndos() < 3 && !justUndid) {
+            ArrayList<Pit> copy = new ArrayList<>();
+            for (Pit pit : currentState) {
+                copy.add(new Pit(pit.getName(), pit.getStones()));
+            }
+            undoStack.push(copy);
+        }
 	}
-	public ArrayList<Pit> "work title" (){
+	public ArrayList<Pit> undo(){
+		if (canUndo()) {
+            currentPlayer.setNumberOfUndos(currentPlayer.getNumberOfUndos() + 1);
+            justUndid = true;
+            return undoStack.pop();
+        }
+        return null;
 	}
 	public void resetUndo(Player nextPlayer) {
 		this.currentPlayer = nextPlayer;
