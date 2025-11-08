c8b27127d1233a7d6073be64fc4fc39be492950c
Glengle Pham <92689013+GlenglePham@users.noreply.github.com>
2025-11-07 23:07:04 -0800

    Update BoardHexagon.java
    I am doing the hexagons

--- BoardHexagon.java
+++ BoardHexagon.java
@@ -22,6 +22,31 @@ public class BoardHexagon implements BoardDesign{
         int mancalaHeight = 300;
         int boardX = 300;
         int boardY = 50;
+
+		
+		// Player A's Mancala (left side)
+        g2.setColor(new Color(160, 82, 45)); 
+        g2.fillOval(50 + boardX, 200, mancalaWidth, mancalaHeight);
+        g2.setColor(Color.BLACK);
+        g2.drawOval(50 + boardX, 200, mancalaWidth, mancalaHeight);
+        
+        // links Player A's mancala coordinates so model can access it
+        pitList.get(6).setPitXCoordinate(50 + boardX);
+        pitList.get(6).setPitYCoordinate(200);
+        pitList.get(6).setPitWidth(mancalaWidth);
+        pitList.get(6).setPitHeight(mancalaHeight);
+
+        // Player B's Mancala (right side)
+        g2.setColor(new Color(160, 82, 45));
+        g2.fillOval(150 + 6 * (pitWidth + spacing) + boardX, 200, mancalaWidth, mancalaHeight);
+        g2.setColor(Color.BLACK);
+        g2.drawOval(150 + 6 * (pitWidth + spacing) + boardX, 200, mancalaWidth, mancalaHeight);
+        
+        // links Player B's mancala coordinates so model can access it
+        pitList.get(13).setPitXCoordinate(150 + 6 * (pitWidth + spacing) + boardX);
+        pitList.get(13).setPitYCoordinate(200);
+        pitList.get(13).setPitWidth(mancalaWidth);
+        pitList.get(13).setPitHeight(mancalaHeight);
 	}
 
 }
